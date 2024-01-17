8fe46748ab735b58ef647daf7725e06d5c8dadb2
Henrique Sampaio <hnrqne@gmail.com>
2024-01-17 00:50:11 -0300

    Spring Boot project

---
+++ src/main/java/com/hnrqne/todobasic/TodobasicApplication.java
@@ -0,0 +1,13 @@
+package com.hnrqne.todobasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodobasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodobasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hnrqne/todobasic/TodobasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnrqne.todobasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodobasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
